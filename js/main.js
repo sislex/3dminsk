beecacc83b0179a0ad2a2368e4dc24316ee686de
Nikolay <work.mail.tola@gmail.com>
2023-11-27 18:46:50 +0300

    1

--- js/main.js
+++ js/main.js
@@ -1,14 +1,20 @@
 const radioButtons = document.querySelectorAll('input[name="btnradio"]');
 const products = document.querySelectorAll('.product');
 radioButtons.forEach((button) => {
-      button.addEventListener('click', () => {
-        const selectedSort = button.getAttribute('data-sort');
-        products.forEach((product) => {
-          if (product.getAttribute('data-type') === selectedSort) {
-            product.style.display = 'block';
-          } else {
-            product.style.display = 'none';
-          }
-        });
+  button.addEventListener('click', () => {
+    const selectedSort = button.getAttribute('data-sort');
+    if (selectedSort === 'all') {
+      products.forEach((product) => {
+        product.style.display = 'block';
       });
-    });
+    } else {
+      products.forEach((product) => {
+        if (product.getAttribute('data-type') === selectedSort) {
+          product.style.display = 'block';
+        } else {
+          product.style.display = 'none';
+        }
+      });
+    }
+  });
+});
